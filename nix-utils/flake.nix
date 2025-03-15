a5f8f4abf6213aff6afb37b3ce9cf1faa2ed7ad9
Theodore Ehrenborg <theodore.ehrenborg@gmail.com>
2025-03-15 12:16:00 +0000

    Add pdftotext and mdformat via nix-direnv

---
+++ nix-utils/flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/807c549feabc";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              poppler_utils
+              mdformat
+            ];
+          };
+        }
+    );
+}
